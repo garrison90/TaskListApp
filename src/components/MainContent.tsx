13b9842a2f108535a62904323de46f8cd133269c
unknown <igorparkhomenko23@gmail.com>
2020-11-01 19:30:53 +0200

    Finish simple app

---
+++ src/components/MainContent.tsx
@@ -0,0 +1,30 @@
+import React, { FC } from "react";
+import { useSelector } from "react-redux";
+
+import SelectList from "./SelectList";
+import { RootState } from "../store/store";
+import AddNewTask from "./AddNewTask";
+import Tasks from "./Tasks";
+
+const MainContent: FC = () => {
+  const selectedList = useSelector(
+    (state: RootState) => state.list.selectedList
+  );
+
+  return (
+    <div className="column is-9">
+      <div className="box">
+        <SelectList />
+        {selectedList && (
+          <>
+            <AddNewTask list={selectedList} />
+            <hr />
+            <Tasks tasks={selectedList.tasks} />
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default MainContent;
